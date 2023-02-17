e9c8a7d58d631a5f9605cf7ee5eb65cd161d9ec5
Rizky Maulana <mrizky9601@gmail.com>
2023-02-17 08:22:39 +0700

    String Split checkpoint

--- coret.rb
+++ coret.rb
@@ -7,6 +7,7 @@ puts long_string.length
 puts long_string.lines.count
 puts "this is value -> #{long_string[0,1]}"
 =end
+=begin
 ls = <<EOS
 It was the best of times,
 It was the worst of times,
@@ -16,3 +17,16 @@ puts ls.length
 puts ls.lines.count
 ls_short = ls[0,1]
 puts ls_short
+=end
+=begin
+ls = '\\\''
+puts ls.size
+puts ls
+puts Math.sqrt(5)
+=end
+
+string = "Bacon, lettuce and tomato"
+puts string[7,3]
+puts string[7..9]
+puts ?a
+puts string.split
